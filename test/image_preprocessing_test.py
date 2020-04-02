825daeb40d91ffa27fdc6065b9e5fd4381ae1889
Heechul90 <heechul4296@gmail.com>
2020-04-02 18:21:06 +0900

    preprocessing

--- test/image_preprocessing_test.py
+++ test/image_preprocessing_test.py
@@ -11,7 +11,47 @@ import utils
 ########################################################################################################################
 from cnn.image_preprocessing import Preprocessing
 
-data_path = 'dataset/image/MonkeySpecies/training'
+data_path = 'dataset/image/MonkeySpecies'
+image_resize = 224
+test_size = 0.3
+
+a = Preprocessing()
+a.setdata(data_path, image_resize, test_size)
+train_data, test_data, train_label, test_label = a.image()
+
+len(train_data)
+len(test_data)
+len(train_label)
+len(test_label)
+
+print(train_data[0].shape)
+print(test_data[0].shape)
+print(train_label[0])
+print(test_label[0])
+
+batch_size = 32
+train_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(train_data, train_label), batch_size=batch_size)
+test_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(test_data, test_label), batch_size=batch_size)
+
+
+################## model
+from mxnet.gluon.model_zoo import vision
+ctx = mx.cpu()
+net = vision.alexnet(classes=10, pretrained=False)
+
+net.initialize(ctx=ctx, init=init.Xavier())
+
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
+utils.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=10)
+# utils.train(train_data, test_data, net, loss, trainer, ctx, num_epochs=1)
+
+
+
+########################################################################################################################
+from cnn.image_preprocessing import Preprocessing
+
+data_path = 'dataset/image/dogs-vs-cats'
 image_resize = 224
 test_size = 0.3
 
@@ -24,41 +64,6 @@ batch_size = 32
 train_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(train_data, train_label), batch_size=batch_size)
 test_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(test_data, test_label), batch_size=batch_size)
 
-for d, l in train_iter:
-    break
-d.shape
-l.shape
-
-
-################## model
-from mxnet.gluon.model_zoo import vision
-ctx = mx.cpu()
-net = vision.alexnet(classes=10, pretrained=False)
-
-net.initialize(ctx=ctx, init=init.Xavier())
-
-loss = gluon.loss.SoftmaxCrossEntropyLoss()
-trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
-utils.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=1)
-# utils.train(train_data, test_data, net, loss, trainer, ctx, num_epochs=1)
-
-
-
-########################################################################################################################
-from cnn.image_preprocessing import Preprocessing
-
-train_path = 'dataset/image/dogs-vs-cats/training'
-test_path = 'dataset/image/dogs-vs-cats/validation'
-image_resize = 224
-batch_size = 32
-
-a = Preprocessing()
-a.setdata(train_path, test_path, image_resize, batch_size)
-train_iter, test_iter = a.image()
-
-for d, l in train_iter:
-    break
-
 
 
 ################## model
@@ -69,7 +74,7 @@ net = vision.alexnet(classes=1, pretrained=False)
 net.initialize(ctx=ctx, init=init.Xavier())
 
 loss = gluon.loss.SigmoidBinaryCrossEntropyLoss()
-
 trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
 utils.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=10)
 # utils.train(train_data, test_data, net, loss, trainer, ctx, num_epochs=1)
+
