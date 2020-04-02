60b71f96f44ba59c4ee6583a8b8482101ae0fca1
Heechul90 <heechul4296@gmail.com>
2020-04-02 16:55:04 +0900

    preprocessing

--- cnn/image_preprocessing.py
+++ cnn/image_preprocessing.py
@@ -1,37 +1,36 @@
-import pandas as pd
-import numpy as np
-from numpy import split
 import mxnet as mx
 from mxnet import gluon, autograd, nd
-
-train_path = 'dataset/image/MonkeySpecies/training'
-test_path = 'dataset/image/MonkeySpecies/validation'
+import numpy as np
+from sklearn.model_selection import train_test_split
 
 
 class Preprocessing():
-    def setdata(self, train_path, test_path, image_resize, batch_size):
-        self.train_path = train_path
-        self.test_path = test_path
+    def setdata(self, data_path, image_resize, test_size):
+        self.data_path = data_path
         self.image_resize = image_resize
-        self.batch_size = batch_size
+        self.test_size = test_size
 
     def image(self):
-        train_path, test_path, image_resize, batch_size = self.train_path, self.test_path, self.image_resize, self.batch_size
+        data_path, image_resize, test_size = self.data_path, self.image_resize, self.test_size
 
         def transformer(data, label):
             data = mx.image.imresize(data, image_resize, image_resize)
             data = mx.nd.transpose(data.astype('float32'), (2, 0, 1)) / 255
+            label = np.float32(label)
             return data, label
 
-        train_iter = gluon.data.DataLoader(
-            gluon.data.vision.datasets.ImageFolderDataset(train_path, transform = transformer),
-            batch_size = batch_size, shuffle = True, last_batch = 'discard')
+        input_data = gluon.data.vision.datasets.ImageFolderDataset(data_path, transform=transformer)
 
-        test_iter = gluon.data.DataLoader(
-            gluon.data.vision.datasets.ImageFolderDataset(test_path, transform = transformer),
-            batch_size = batch_size, shuffle = True, last_batch = 'discard')
+        img_data = []
+        img_label = []
+        for d, l in input_data:
+            img_data.append(d)
+            img_label.append(l)
 
-        return train_iter, test_iter
+
+        train_data, test_data, train_label, test_label = train_test_split(img_data, img_label, test_size=test_size)
+
+        return train_data, test_data, train_label, test_label
 
 
 
--- test/image_preprocessing_test.py
+++ test/image_preprocessing_test.py
@@ -1,37 +1,75 @@
 import pandas as pd
 import numpy as np
-from numpy import split
+from sklearn.model_selection import train_test_split
 import mxnet as mx
 from mxnet import gluon, autograd, nd
-
-train_path = 'dataset/image/MonkeySpecies/training'
-test_path = 'dataset/image/MonkeySpecies/validation'
-
-
-class Preprocessing():
-    def setdata(self, train_path, test_path, image_resize, batch_size):
-        self.train_path = train_path
-        self.test_path = test_path
-        self.image_resize = image_resize
-        self.batch_size = batch_size
-
-    def image(self):
-        train_path, test_path, image_resize, batch_size = self.train_path, self.test_path, self.image_resize, self.batch_size
-
-        def transformer(data, label):
-            data = mx.image.imresize(data, image_resize, image_resize)
-            data = mx.nd.transpose(data.astype('float32'), (2, 0, 1)) / 255
-            return data, label
-
-        train_iter = gluon.data.DataLoader(
-            gluon.data.vision.datasets.ImageFolderDataset(train_path, transform = transformer),
-            batch_size = batch_size, shuffle = True, last_batch = 'discard')
-
-        test_iter = gluon.data.DataLoader(
-            gluon.data.vision.datasets.ImageFolderDataset(test_path, transform = transformer),
-            batch_size = batch_size, shuffle = True, last_batch = 'discard')
-
-        return train_iter, test_iter
+from mxnet import init
+import utils
 
 
 
+########################################################################################################################
+from cnn.image_preprocessing import Preprocessing
+
+data_path = 'dataset/image/MonkeySpecies/training'
+image_resize = 224
+test_size = 0.3
+
+a = Preprocessing()
+a.setdata(data_path, image_resize, test_size)
+train_data, test_data, train_label, test_label = a.image()
+
+
+batch_size = 32
+train_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(train_data, train_label), batch_size=batch_size)
+test_iter = gluon.data.DataLoader(gluon.data.ArrayDataset(test_data, test_label), batch_size=batch_size)
+
+for d, l in train_iter:
+    break
+d.shape
+l.shape
+
+
+################## model
+from mxnet.gluon.model_zoo import vision
+ctx = mx.cpu()
+net = vision.alexnet(classes=10, pretrained=False)
+
+net.initialize(ctx=ctx, init=init.Xavier())
+
+loss = gluon.loss.SoftmaxCrossEntropyLoss()
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
+utils.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=1)
+# utils.train(train_data, test_data, net, loss, trainer, ctx, num_epochs=1)
+
+
+
+########################################################################################################################
+from cnn.image_preprocessing import Preprocessing
+
+train_path = 'dataset/image/dogs-vs-cats/training'
+test_path = 'dataset/image/dogs-vs-cats/validation'
+image_resize = 224
+batch_size = 32
+
+a = Preprocessing()
+a.setdata(train_path, test_path, image_resize, batch_size)
+train_iter, test_iter = a.image()
+
+for d, l in train_iter:
+    break
+
+
+
+################## model
+from mxnet.gluon.model_zoo import vision
+ctx = mx.cpu()
+net = vision.alexnet(classes=1, pretrained=False)
+
+net.initialize(ctx=ctx, init=init.Xavier())
+
+loss = gluon.loss.SigmoidBinaryCrossEntropyLoss()
+
+trainer = gluon.Trainer(net.collect_params(), 'sgd', {'learning_rate': 0.01})
+utils.train(train_iter, test_iter, net, loss, trainer, ctx, num_epochs=10)
+# utils.train(train_data, test_data, net, loss, trainer, ctx, num_epochs=1)
